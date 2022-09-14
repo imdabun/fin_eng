30d093495a88323d7c37a815a162f6d94465b164
Gan Yang <gany1@andrew.cmu.edu>
2022-09-14 11:02:32 -0400

    added log simulation

--- sde.py
+++ sde.py
@@ -55,10 +55,27 @@ class EulerParam(Param):
         return self._last
 
 
+class LogEulerParam(EulerParam):
+    """A node of Non-negative Euler's discretization of an SDE."""
+
+    name = "Log Euler Param"
+
+    def eval(self):
+        # just return cached value if node is not dirty
+        if not self._dirty:
+            return self._last
+        # if not, calculate using SDE
+        S_t = self._S_t.eval()
+        dt = self._dt.eval()
+        self._last = S_t * np.exp(self._mu(S_t, self._t) * dt + self._v(S_t, self._t) * math.sqrt(dt) * self._z)
+        self._dirty = False
+        return self._last
+
+
 class EulerSimulation:
     """Wrapper for Euler's discretation Monte Carlo Simulation."""
 
-    def __init__(self, mu, v, S_0, T, dt, n_sims):
+    def __init__(self, mu, v, S_0, T, dt, n_sims, param=EulerParam):
         """
         mu      Lambda function input for Euler Param
         v       Lambda function input for Euler Param
@@ -74,7 +91,7 @@ class EulerSimulation:
         self.nodes = [last]
         self.Z = np.random.normal(size=[T, n_sims])
         for i in range(T):
-            last = EulerParam(mu, v, last, i+1, dt, self.Z[i, 0])
+            last = param(mu, v, last, i+1, dt, self.Z[i, 0])
             self.nodes.append(last)
         self.results = np.empty([T+1, n_sims])
         self.results[0, :] = S_0.eval()
@@ -85,10 +102,7 @@ class EulerSimulation:
                 self.nodes[j+1].z = self.Z[j, i]
                 self.results[j+1, i] = self.nodes[j+1].eval()
 
-    def run_new(self):
-        z = np.random.normal(size=self._T+1)
-        z[0] = self._S_0.eval()
-        for i in range(1, self._T+1):
-            self.nodes[i].z = z[i]
-            z[i] = self.nodes[i].eval()
-        return z
+    def run_new(self, n_sims):
+        self._n = n_sims
+        self.Z = np.random.normal(size=[self._T, n_sims])
+        self.run_all()
